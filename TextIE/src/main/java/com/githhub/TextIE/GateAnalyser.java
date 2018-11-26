ec70d9d8cf7127702d5f845870007faf396eee8d
Kishore Kasaraneni <kasaraneni.kishore@gmail.com>
2018-11-26 07:37:23 +0530

    Text Information Extraction
    TextIE to explore GATE tool

---
+++ TextIE/src/main/java/com/githhub/TextIE/GateAnalyser.java
@@ -0,0 +1,13 @@
+package com.githhub.TextIE;
+
+/**
+ * Hello world!
+ *
+ */
+public class GateAnalyser 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
